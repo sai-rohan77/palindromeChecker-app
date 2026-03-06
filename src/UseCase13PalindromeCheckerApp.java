e4623579c96c59bdccf6b1856f0e1d4e8176bcfa
johnComputer <johnComputer@gmail.com>
2026-03-06 10:43:34 +0530

    UC13: Performance Comparison

---
+++ src/UseCase13PalindromeCheckerApp.java
@@ -0,0 +1,22 @@
+public class UseCase13PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String input = "level";
+
+        // Using the strategy created earlier
+        PalindromeStrategy strategy = new StackStrategy();
+
+        long startTime = System.nanoTime();
+
+        boolean result = strategy.check(input);
+
+        long endTime = System.nanoTime();
+
+        long executionTime = endTime - startTime;
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + result);
+        System.out.println("Execution Time : " + executionTime + " ns");
+    }
+}
